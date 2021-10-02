8deed3456509b905a335b56df0134f28154cd1bc
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-10-01 22:53:33 -0500

    6 - Eliminando elementos del Array

--- array/array.js
+++ array/array.js
@@ -16,6 +16,28 @@ class MyArray {
         return this.data;
     }
 
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1]
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) { 
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+
+    shiftIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1]
+        this.length--;
+    }
+
+
 }
 
 const myArray = new MyArray();
