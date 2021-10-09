491dd99ad599125dcbde0d20fd9eb5dfe3b30d0c
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-10-09 16:53:10 -0500

    15 - Double Linked List

--- linkedList/dobly.js
+++ linkedList/dobly.js
@@ -27,6 +27,77 @@ class MyDoublyLinkedList {
         return this;
     }
 
+    prepend(value) {
+        const newNode = new Node(value);
+        this.head.prev = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+
+    insert(index, value) {
+
+        if (index >= this.length) {
+            return this.append(value);
+        }
+
+        if (index === 1) {
+            return this.prepend(value);
+        }
+
+        const newNode = Node(value);
+        const prevPointer = this.getTheIndex(index - 1);
+        const nextPointer = this.getTheIndex(index + 1);
+
+        newNode.prev = prevPointer;
+        newNode.next = nextPointer;
+
+        nextPointer.prev = newNode;
+        prevPointer.next = newNode;
+
+        this.length++;
+        return this;
+
+    }
+
+    remove(index) {
+
+        if (index >= this.length) {
+            console.log("El indice no existe")
+            return
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            this.head.prev = null;
+            this.length--;
+            return this;
+        }
+
+        const prevNode = this.getTheIndex(index - 1);
+        const nextNode = this.getTheIndex(index + 1);
+
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+
+        this.length--;
+        return this;
+
+    }
+
+    getTheIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+
+        return currentNode;
+    }
+
 }
 
 let myLinkedList = new MyDoublyLinkedList(1);
