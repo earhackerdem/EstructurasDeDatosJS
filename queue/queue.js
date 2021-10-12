889970f133d57123afae7fd70be0069159e17aa8
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-10-12 12:15:01 -0500

    19 - Construyendo un Queue

---
+++ queue/queue.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    dequeue() {
+
+        if (this.length === 0) {
+            throw new Error("The Queue is empty");
+        }
+
+        if (this.length === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            this.first = this.first.next;
+        }
+
+        this.length--;
+        return this;
+    }
+
+    peek() {
+        return this.first;
+    }
+}
+
+const myQueue = new Queue;
